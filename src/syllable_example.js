8c886fe1c9923686bc6bb7bcf3db4031f1378cfc
kmoon2437 <kmoon2437@gmail.com>
2022-02-16 16:26:03 +0900

    변환이 완료된 결과물에 후리가나(ruby문자) 구문이 그대로 남아있는 문제 수정

---
+++ src/syllable_example.js
@@ -0,0 +1,9 @@
+[
+    ['stdSyll'], // 한 음절에 들어가는 요소들을 배열로 담음
+    
+    // [rubyS]{rubyContent}yll
+    // ruby: 위에 글자, length: 적용될 글자의 길이
+    // 객체 2개가 연속으로 올 수는 없음(문자열 2개가 연속으로 올 수는 있지만)
+    [{ ruby:'rubyContent',length:5 },'rubySyll'],
+    '...'
+]
